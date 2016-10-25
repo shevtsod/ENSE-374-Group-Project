c678da8f519fdc4e548861de9d02a9cce4e3a99b
shevtsod <daniel.shevtsov@gmail.com>
2016-10-24 18:36:38 -0600

    Add Main class

---
+++ src/com/Main.java
@@ -0,0 +1,15 @@
+/**
+ * CLASS:       ENSE 374
+ * AUTHOR:      Group 9
+ * PURPOSE:
+ *      Main entry point for the application.
+ *      main() is called when the application starts.
+ */
+
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+	//TODO: Add main() body
+    }
+}
