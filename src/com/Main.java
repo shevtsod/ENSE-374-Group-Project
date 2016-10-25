6342e537eca285fdbd92e7b6d5e872b9892896f0
shevtsod <daniel.shevtsov@gmail.com>
2016-10-24 19:16:44 -0600

    Add Main.java comments

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,7 +1,7 @@
 /**
- * FILENAME:    Main.java
- * COURSE:      ENSE 374
- * AUTHOR:      Group 9
+ * FILENAME:        Main.java
+ * COURSE:          ENSE 374
+ * AUTHOR:          Group 9
  *
  * PURPOSE:
  *      Main entry point for the application.
@@ -10,6 +10,15 @@
 
 package com;
 
+/**
+ * class Main
+ *
+ * Serves as primary entry point for program.
+ * Initializes SessionManager, Database, and BackupManager objects to control
+ * the application's data and logic flow.
+ *
+ * @author          Group 9
+ */
 public class Main {
     public static void main(String[] args) {
 	//TODO: Add main() body
