7cfe80bc4bd2a5a259c03668535f75302dc8d760
shevtsod <daniel.shevtsov@gmail.com>
2016-10-25 14:03:10 -0600

    Add package-info for shevtsod package Add SessionManager class Update Main

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,10 +2,6 @@
  * FILENAME:        Main.java
  * COURSE:          ENSE 374
  * AUTHOR:          Group 9
- *
- * PURPOSE:
- *      Main entry point for the application.
- *      main() is called when the application starts.
  */
 
 package com;
---
+++ src/com/shevtsod/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * <h1>shevtsod</h1>
+ * Contains implementations for the following classes:
+ * <ul>
+ *      <li>SessionManager</li>
+ *      <li>StateManager</li>
+ *      <li>StateType</li>
+ *      <li>UserType</li>
+ * </ul>
+ * @author Daniel Shevtsov
+ */
+package com.shevtsod;
+
+//Empty file -- Purpose: define Javadoc for package "shevtsod".
